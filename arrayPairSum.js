503e59a56ee42bcd432e8642e721b53999d9a4f9
nathan-builds <61122637+nathan-builds@users.noreply.github.com>
2022-09-27 16:07:04 -0400

    Add files via upload

---
+++ arrayPairSum.js
@@ -0,0 +1,32 @@
+/**
+ * Given an integer array nums of 2n integers, group these integers into n pairs 
+ * (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+Example 1:
+
+Input: nums = [1,4,3,2]
+Output: 4
+Explanation: All possible pairings (ignoring the ordering of elements) are:
+1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+So the maximum possible sum is 4.
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function (nums) {
+  let sum = 0;
+  nums.sort((a, b) => {
+    return a - b;
+  });
+
+  var i = nums.length - 2;
+  console.log(i);
+  while (i >= 0) {
+    sum += nums[i];
+    i -= 2;
+  }
+  return sum;
+};
